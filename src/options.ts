f0ec2d7646040a6a4697c0d86b7e1da6bdea8f9a
remorses <beats.by.morse@gmail.com>
2020-11-24 17:27:20 +0100

    options: cache it

--- src/options.ts
+++ src/options.ts
@@ -2,9 +2,14 @@ import fs from 'fs'
 import path from 'path'
 import strip from 'strip-json-comments'
 
+let options
+
 export const getOptions = (
     cwd?: string,
 ): { jsxFactory?: string; jsxFragment?: string; target?: string } => {
+    if (options) {
+        return options
+    }
     cwd = cwd || process.cwd()
     const tsconfig = path.resolve(cwd, 'tsconfig.json')
     if (fs.existsSync(tsconfig)) {
@@ -12,11 +17,14 @@ export const getOptions = (
             strip(fs.readFileSync(tsconfig).toString() || 'null'),
         )
         if (data) {
-            return {
+            options = {
                 jsxFactory: data.compilerOptions?.jsxFactory,
                 jsxFragment: data.compilerOptions?.jsxFragmentFactory,
                 target: data.compilerOptions?.target,
             }
+            return options
         }
     }
+    options = {}
+    return options
 }
